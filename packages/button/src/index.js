7e526b578bbb3ae6b75c2fd52daae83d2428661b
fredojbg <alfredo@frncubo.com>
2020-07-14 21:16:51 -0300

    feat: add configurations

---
+++ packages/button/src/index.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: #fff;
+  background: red;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 300;
+  padding: 9px 36px;
+  border-radius: 4px;
+`;
+
+
+const ButtonAzul = styled.button`
+  color: #fff;
+  background: blue;
+  cursor: pointer;
+  font-size: 16rem;
+  font-weight: 300;
+  padding: 19px 16px;
+  border-radius: 14px;
+`;
+
+
+export { Button, ButtonAzul };
